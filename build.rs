bf6bbef53feaf104e36cd0b87001c1ad7b184dc6
taiyangkao <taiyangkaorou@gmail.com>
2026-03-19 09:58:37 +0800

    Refactor protobuf files into domain directories

--- build.rs
+++ build.rs
@@ -2,8 +2,23 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let proto_files = ["proto/client_server.proto", "proto/internal_engine.proto"];
+    let tracked_proto_files = [
+        "proto/client_server.proto",
+        "proto/client/common/enums.proto",
+        "proto/client/common/types.proto",
+        "proto/client/actions.proto",
+        "proto/client/events.proto",
+        "proto/client/settlement.proto",
+        "proto/client/session.proto",
+        "proto/internal_engine.proto",
+        "proto/engine/common/enums.proto",
+        "proto/engine/common/types.proto",
+        "proto/engine/validation.proto",
+        "proto/engine/scoring.proto",
+        "proto/engine/service.proto",
+    ];
 
-    for proto_file in proto_files {
+    for proto_file in tracked_proto_files {
         println!("cargo:rerun-if-changed={proto_file}");
     }
 
